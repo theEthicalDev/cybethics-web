20db0b4e0c0947475b3862cbcce0c2b41366bdda
theEthicalDev <djordje.karadzic@cybethics.com>
2023-08-31 09:29:37 +0200

    Completed UNSDG and contact form

--- Cybethics Website/functions.js
+++ Cybethics Website/functions.js
@@ -1,6 +1,6 @@
 function cybethicsNameContinue() {
     const continuedElems = Array.from(document.getElementsByClassName('continued'));
-    const button = document.getElementsByClassName('cyber-ethical-solutions-button')[0];
+    const button = document.getElementById('cyber-ethical-solutions-button');
     button.setAttribute('disabled', true);
     if (continuedElems.length > 0) {
         setTimeout(function () {
@@ -24,22 +24,13 @@ function cybethicsNameContinue() {
                 titleElem.textContent = '_';
                 let counter = 1;
                 for (let i = 0; i < alphabet.length; i++) {
-                    let counter = 1;
-                    while (counter <= 2) {
-                        console.log(counter);
-                        counter++;
-                    }
                     titleElem.textContent = alphabet[i];
                     if (titleElem.textContent === targetLetter) {
-                        break; // Stop the loop when the target letter is reached
+                        break;
                     }
                 }
                 titleElem.parentElement.parentElement.classList.add('continued');
             }, 500);
-            // setTimeout(function () {
-            //     titleElem.parentElement.parentElement.classList.add('continued');
-            //     titleElem.textContent = oldValue;
-            // }, 500);
         }
         setTimeout(function () {
             for (const cyberEthicalSolutionsContainer of allCyberEthicalSolutionsElems) {
@@ -56,4 +47,53 @@ function cybethicsNameContinue() {
     }
 }
 
+// Rotating text above the contact form
+document.addEventListener("DOMContentLoaded", function() {
+    let words = document.querySelectorAll(".word");
+    
+    let wordArray = Array.from(words);
 
+    wordArray.forEach(word => {
+        let letters = word.textContent.split("");
+        word.textContent = "";
+
+        letters.forEach(letter => {
+            let span = document.createElement("span");
+            span.textContent = letter;
+            span.style.minWidth = "0.2em";
+            span.className = "letter";
+            word.appendChild(span);
+        });
+    });
+
+    let currentWordIndex = 0;
+    let maxWordIndex = wordArray.length - 1;
+    wordArray[currentWordIndex].style.opacity = "1";
+
+    let rotateText = () => {
+        let currentWord = wordArray[currentWordIndex];
+        let nextWord =
+            currentWordIndex === maxWordIndex ? wordArray[0] : wordArray[currentWordIndex + 1];
+
+        Array.from(currentWord.children).forEach((letter, i) => {
+            console.log(i);
+            setTimeout(() => {
+                letter.className = "letter out";
+            }, i * 80);
+        });
+
+        nextWord.style.opacity = "1";
+        Array.from(nextWord.children).forEach((letter, i) => {
+            letter.className = "letter behind";
+            setTimeout(() => {
+                letter.className = "letter in";
+            }, 340 + i * 80);
+        });
+
+        currentWordIndex =
+            currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+    };
+
+    rotateText();
+    setInterval(rotateText, 4000);
+});
--- Cybethics Website/planet.js
+++ Cybethics Website/planet.js
@@ -139,3 +139,17 @@ function addOptionClick() {
         });
     }
 }
+
+var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+        invert: false,
+    },
+    //autoHeight: true,
+    pagination: {
+        el: '.blog-slider__pagination',
+        clickable: true,
+    }
+});
